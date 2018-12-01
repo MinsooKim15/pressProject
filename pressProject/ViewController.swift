9cbe8f4f075caa9d52d8ab7e32fbd06a4db4be4b
minsookim <minsookim0615@gmail.com>
2018-12-01 14:12:44 +0900

    Initial Commit

---
+++ pressProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pressProject
+//
+//  Created by minsoo kim on 01/12/2018.
+//  Copyright © 2018 minsoo kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
